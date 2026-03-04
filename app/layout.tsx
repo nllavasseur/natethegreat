d4d31ffde5129dcade91f1c8ec3ec504e8a09135
nllavasseur <nathan@vasseurfencing.com>
2026-03-04 08:32:31 -0500

    Revert "Enable viewport-fit cover and paint estimates footer to bottom"
    This reverts commit ea61e592a19a404e906487e654d6eef3e507d642.

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,7 @@ export const viewport: Viewport = {
   initialScale: 1,
   minimumScale: 1,
   maximumScale: 1,
-  userScalable: false,
-  viewportFit: "cover"
+  userScalable: false
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
